aee9ffeebe06dd8982b510ccd06c813f5c4abc30
domagoj.vukovic <off.vukovic@gmail.com>
2021-05-28 19:32:13 +0200

    add bash public

---
+++ .bash_public.bash
@@ -0,0 +1,10 @@
+# Add to .bashrc "source ~/.bash_public.bash"
+
+source ~/.bash_functions.bash
+source ~/.bash_aliases.bash
+
+# Init git keys
+eval $(gnome-keyring-daemon --start)
+export SSH_AUTH_SOCK
+export GNOME_KEYRING_PID
+export GNOME_KEYRING_CONTROL
