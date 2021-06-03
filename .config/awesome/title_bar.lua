940fac98b8ccdac11df5576864f5df5ce6ec4d54
domagoj.vukovic <off.vukovic@gmail.com>
2021-06-03 22:54:54 +0200

    awesome/move title bar to separate file

---
+++ .config/awesome/title_bar.lua
@@ -0,0 +1,35 @@
+local keys = require('keys')
+local awful = require("awful")
+local wibox = require("wibox")
+
+client.connect_signal("request::titlebars", function(client)
+    local left_section = { 
+        layout  = wibox.layout.fixed.horizontal,
+        buttons = keys.client_toolbar_buttons(client)
+    }
+
+    local midddle_section = { 
+        buttons = keys.client_toolbar_buttons(client),
+        layout  = wibox.layout.flex.horizontal
+    }
+
+    local right_section = {
+        layout = wibox.layout.fixed.horizontal(),
+        awful.titlebar.widget.floatingbutton(client),
+        awful.titlebar.widget.maximizedbutton(client),
+        awful.titlebar.widget.closebutton(client),
+    }
+
+    local style = {
+        bg_normal = '#1a1a1b85',
+        bg_focus = '#1a1a1bab',
+        size = 20,
+    }
+
+    awful.titlebar(client, style) : setup {
+        left_section,
+        midddle_section,
+        right_section,
+        layout = wibox.layout.align.horizontal
+    }
+end)
