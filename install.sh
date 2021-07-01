02ee3b015b68a212dfac06caab1cc3d9f0e9396b
domagoj.vukovic <off.vukovic@gmail.com>
2021-07-01 22:44:51 +0200

    intall/add docker

--- install.sh
+++ install.sh
@@ -97,6 +97,18 @@ install_other() {
     pip install ueberzug             # Ranger image preview
 }
 
+install_docker() {
+    sudo apt-get update
+    sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo \
+        "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io
+}
+
 install_dev_tools() {
 
     # Snaps
@@ -106,6 +118,8 @@ install_dev_tools() {
 
     # APTs
     sudo apt install alacritty
+
+    install_docker
 }
 
 install_media_tools() {
