e83cb51be385eb7faf4733f5d318570fc6b1b7f2
domagoj.vukovic <off.vukovic@gmail.com>
2021-07-04 02:00:59 +0200

    add more screen layouts

---
+++ .screenlayout/single-personal.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --off --output DP-2 --off --output DP-3 --off --output DVI-I-5-4 --off --output DVI-I-4-3 --off --output eDP-1-0 --mode 1280x720 --pos 0x0 --rotate normal --output DVI-I-2-1 --off --output DVI-I-3-2 --off
