6a21bc6920281e4ca5814c02e5f750ed233162da
domagoj.vukovic <off.vukovic@gmail.com>
2021-08-31 17:19:49 +0200

    install/ref: remove yarn emoji setting

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 ################################################################################################
 
 install_npm_packages() {
-    npm i -g yarn -y && yarn --emoji true             # Node package manager
+    npm i -g yarn -y                                  # Node package manager
     npm i -g eslint_d -y                              # JS Linter/Formatter
     npm i -g prettier -y                              # JS Linter/Formatter
     npm i -g markdownlint -y                          # Markdown linter
