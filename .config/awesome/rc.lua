9fc39f93df5a1ef85e48332ea6db8cd44354c8af
domagoj.vukovic <off.vukovic@gmail.com>
2021-09-15 18:10:58 +0200

    awesome/fix: client focus on tag change

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -31,3 +31,21 @@ end)
 client.connect_signal("mouse::enter", function(client)
     client:emit_signal("request::activate", "mouse_enter", { raise = false })
 end)
+
+-- Focus client where mouse is
+-- https://stackoverflow.com/a/52802148/11266179
+function focus_client_under_mouse()
+    gears.timer({
+        timeout = 0.1,
+        autostart = true,
+        single_shot = true,
+        callback = function()
+            local n = mouse.object_under_pointer()
+            if n ~= nil and n ~= client.focus then
+                client.focus = n
+            end
+        end,
+    })
+end
+
+screen.connect_signal("tag::history::update", focus_client_under_mouse)
